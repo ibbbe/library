eaaa0c3749c6735d1774a1f451cb8a0ddd477774
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-11-05 16:42:37 +0100

    Finish addBookToLibrary() and submission in myLibrary[]

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
+
+
+document.querySelector("#submit")
+    .addEventListener('click', (event) => {
+        event.preventDefault();
+        addBookToLibrary();
+        document.querySelector('form').reset();
+    });
 
 /*addBookToLibrary('The hobbit', 'J.R.R: Tolkien', 295, 'read');
 addBookToLibrary('Hamlet', 'William Shakespeare', 143, 'read') */
@@ -18,5 +31,3 @@ addBookToLibrary('Hamlet', 'William Shakespeare', 143, 'read') */
 function displayBooksInLibrary() {
     myLibrary.forEach((book) => console.log(book));
 }
-
-displayBooksInLibrary();
