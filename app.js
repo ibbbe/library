5a182aebac69ffbf416b430c16996d9ed39dfd4a
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-11-05 09:50:27 +0100

    Add function displayBooksInLibrary, style button

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read  = read;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -12,8 +12,8 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-addBookToLibrary('The hobbit', 'J.R.R: Tolkien', 295, 'read many times');
-addBookToLibrary('Hamlet', 'William Shakespeare', 143, 'once read')
+/*addBookToLibrary('The hobbit', 'J.R.R: Tolkien', 295, 'read');
+addBookToLibrary('Hamlet', 'William Shakespeare', 143, 'read') */
 
 function displayBooksInLibrary() {
     myLibrary.forEach((book) => console.log(book));
