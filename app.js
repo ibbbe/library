ac0d00ed9763ff007d18bdbdd0c6eebf4da388ab
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-11-07 09:25:11 +0100

    Add finished project library

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ function addBookToLibrary() {
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').checked;
-     console.log(read);
+
     const book = new Book(title, author, pages, read);
+
     myLibrary.push(book);
     displayBooksInLibrary();
 }
-
 
 document.querySelector("#submit")
     .addEventListener('click', (event) => {
@@ -26,46 +26,48 @@ document.querySelector("#submit")
         document.querySelector('form').reset();
     });
 
-/*addBookToLibrary('The hobbit', 'J.R.R: Tolkien', 295, 'read');
-addBookToLibrary('Hamlet', 'William Shakespeare', 143, 'read') */
-
 function displayBooksInLibrary() {
-    const { title, author, pages, isRead } =
+    const { title, author, pages, read } =
         myLibrary[myLibrary.length - 1];
 
     const tbody = document.querySelector("tbody");
+    const tableRow = document.createElement('tr');
 
     const tDTitle = document.createElement('td');
-    tDTitle.classList.add((myLibrary.length - 1));
     tDTitle.textContent = title;
-    tbody.appendChild(tDTitle);
+    tableRow.appendChild(tDTitle);
 
     const tDAuthor = document.createElement('td');
-    tDAuthor.classList.add(myLibrary.length - 1);
     tDAuthor.textContent = author;
-    tbody.appendChild(tDAuthor);
+    tableRow.appendChild(tDAuthor);
 
     const tDPages = document.createElement('td');
-    tDPages.classList.add(myLibrary.length - 1);
     tDPages.textContent = pages;
-    tbody.appendChild(tDPages);
+    tableRow.appendChild(tDPages);
 
     const tDRead = document.createElement('td');
-    tDRead.classList.add(myLibrary.length - 1);
-    tDRead.textContent = isRead;
-    tbody.appendChild(tDRead);
+    const check = document.createElement('INPUT');
+    check.setAttribute('type', 'checkbox');
+    tDRead.appendChild(check);
+    if (read === false) {
+        check.checked = false;
+    } else {
+        check.checked = true;
+    }
+    tableRow.appendChild(tDRead);
 
     const tdRemoveBook = document.createElement('td');
     const removeButton = document.createElement('button');
     removeButton.classList.add(myLibrary.length - 1);
     removeButton.classList.add('removeButton')
     removeButton.textContent = 'Remove book';
-    tbody.appendChild(removeButton);
+    tableRow.appendChild(removeButton);
 
-    document.querySelector('.removeButton')
-    .addEventListener('click', (event) => {
+    tbody.appendChild(tableRow);
+
+
+    removeButton.addEventListener('click', (event) => {
         const table = document.querySelector('#books');
-        table.removeChild(tbody);
-        //event.preventDefault();
+        tbody.removeChild(tableRow);
     })
 }
